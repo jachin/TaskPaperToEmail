39add67eeefb6edda75b0a9856ed65ad23514396
Jachin Rupe <jachin.rupe@gmail.com>
2013-05-20 17:39:47 -0500

    Getting the wrap.py script inline with the python linter.

--- wrap.py
+++ wrap.py
@@ -1,34 +1,36 @@
 import re
 import sys
 
+
 def wrap(text, width):
-	pattern = re.compile('^([\s\-]+)(.+$)')
-	wrapped_text = ''
 
-	for line in text.split('\n'):
-		if len(line) < width:
-			wrapped_text += line + '\n'
-		else:
-			m = pattern.match(line)
+    pattern = re.compile('^([\s\-]+)(.+$)')
+    wrapped_text = ''
 
-			indent = m.group(1)
-			content = m.group(2)
+    for line in text.split('\n'):
+        if len(line) < width:
+            wrapped_text += line + '\n'
+        else:
+            m = pattern.match(line)
 
-			indent_len = len( indent )
+            indent = m.group(1)
+            content = m.group(2)
 
-			current_line = indent
+            indent_len = len(indent)
 
-			for word in content.split():
-				if len(word) + len(current_line) + 1 > width:
-					wrapped_text += current_line + '\n'
-					current_line = ' ' * indent_len
-					current_line += ' ' + word
-				else:
-					current_line += ' ' + word
+            current_line = indent
 
-			if len(current_line) > 0:
-				wrapped_text += current_line + '\n'
-	return wrapped_text
+            for word in content.split():
+                if len(word) + len(current_line) + 1 > width:
+                    wrapped_text += current_line + '\n'
+                    current_line = ' ' * indent_len
+                    current_line += ' ' + word
+                else:
+                    current_line += ' ' + word
+
+            if len(current_line) > 0:
+                wrapped_text += current_line + '\n'
+    return wrapped_text
 
 msg = sys.stdin.read()
 
